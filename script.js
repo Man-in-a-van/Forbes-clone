8d503c6fa29bf585a94ed44cc300ded78b97026b
Man-in-a-van <73036886+Man-in-a-van@users.noreply.github.com>
2023-07-03 17:20:01 +0100

    Added functionality to breaking news sliders
    Buttons next to breaking news now slide the articles over and hide buttons that aren't usable.

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-function toggleHidden() {
+function toggleHidden() { //adds classes to hide certain articles and show which articles are selected
     document.getElementById("pop-articles").classList.toggle("hidden");
     document.getElementById("editors-articles").classList.toggle("hidden");
     document.getElementById("popular").classList.toggle("selected");
     document.getElementById("editor-picks").classList.toggle("selected");
 }
 
-document.getElementById("popular").onclick = function() {
-    if (document.getElementById("pop-articles").classList.contains("hidden")) {
-        toggleHidden();
+document.getElementById("popular").onclick = function() { //when button is clicked
+    if (document.getElementById("pop-articles").classList.contains("hidden")) { //checkcs if article list is already selected
+        toggleHidden(); //changes article list if not already selected
     }
 }
 
-document.getElementById("editor-picks").onclick = function() {
-    if (document.getElementById("editors-articles").classList.contains("hidden")) {
-        toggleHidden();
+document.getElementById("editor-picks").onclick = function() { //when button is clicked
+    if (document.getElementById("editors-articles").classList.contains("hidden")) { //checkcs if article list is already selected
+        toggleHidden(); //changes article list if not already selected
     }
 }
 
-const animate_hor = document.querySelectorAll('.animate-on-scroll-horizontal');
-const animate_vert = document.querySelectorAll('.animate-on-scroll-vertical');
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            if (entry.target.classList.contains("animate-on-scroll-horizontal")){
-                entry.target.classList.add('animate-horizontal');
+const animate_hor = document.querySelectorAll('.animate-on-scroll-horizontal'); //creates an arrow with every element that has a class of "animate-on-scroll-horizontal
+const animate_vert = document.querySelectorAll('.animate-on-scroll-vertical');//creates an arrow with every element that has a class of "animate-on-scroll-vertical
+
+const observer = new IntersectionObserver((entries) => { //creates an observer
+    entries.forEach((entry) => { 
+        if (entry.isIntersecting) { //checks if certain elements are visible / has been scrolled down to
+            if (entry.target.classList.contains("animate-on-scroll-horizontal")){ 
+                entry.target.classList.add('animate-horizontal'); //adding the animate class to trigger the animation
             } else{
                 entry.target.classList.add('animate-vertical');
             }
         }
     })
 }, {
-    threshold: 1
+    threshold: 1 //waits untill the element is 100% in view before animating
 });
 
-for (let i = 0; i < animate_hor.length; i++) {
+for (let i = 0; i < animate_hor.length; i++) { //looping through array of elements we want to observe to animate
     const el = animate_hor[i];
     observer.observe(el);
 }
@@ -41,3 +42,12 @@ for (let i = 0; i < animate_vert.length; i++) {
     const el = animate_vert[i];
     observer.observe(el);
 }
+
+function toggleSlide(){
+    document.getElementById("sliders").classList.toggle("animate-slide"); //triggers slide animation
+    document.getElementById("slide-right").classList.toggle("no-opacity"); //hides/unhides the slide buttons
+    document.getElementById("slide-left").classList.toggle("no-opacity");
+}
+
+document.getElementById("slide-right").onclick = toggleSlide;
+document.getElementById("slide-left").onclick = toggleSlide;
